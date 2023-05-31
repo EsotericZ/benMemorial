973bdaf42cb2c69564801a26f08d81231881f7aa
EsotericZ <cjsand03@gmail.com>
2023-05-31 08:15:20 -0600

    home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,26 @@
 export const Home = () => {
     return (
         <>
-            <h1>
-                Home
-            </h1>
+            <div className="d-flex justify-content-center mt-2">
+                <h1>Arrangements</h1>
+            </div>
+            <div className="d-flex justify-content-center text-center m-2">
+                Visitiation will be held Sunday, June 4th, 2023 at the Clark Legacy Center from 5-7pm. All are welcome to come share stories, be with family and celebrate the life of Ben.
+            </div>
+            <div className="d-flex justify-content-center text-center m-2">
+                The Funeral will be held Sunday, June 4th, 2023 at the Clark Legacy Center at 7pm
+            </div>
+            <div className="d-flex justify-content-center text-center m-2">
+                601 E Brannon Rd, Nicholasville, KY 40356
+            </div>
+
+            <div className="d-flex justify-content-center mt-2">
+                <img
+                    src='https://lh3.googleusercontent.com/WcNixBE2ZWLcnljDV5pPjLTZYr1tOIYI2UCwfvCpxqqKr6cHZbImkd8a400WWeIYLZSxZ0mP1OhwzJDm0ufMJQiiQYBaN0QGcoM6IoFMxsRcRg3JFm2HqUf8k1Iub993x3s1LQ4xWh-pLvkf9hkHbkgXjGDBHgI5GBvnoYe_U8AfM9EMKU0g84pJ3waPUhQLjil5JiWxtnkFPLaDDMo-Zz_YNHPIaHOKbzUQg2OXQ5YYNK9jq-o3xriyIHQqL779NQEIIyZEZbgEFZwp2HEUAIB2T0m1g2-PXgrlN9sU7fM6q62QjtrGjalwb-IBBvSMji4Z6kDb4XytbMN977X3AaASywhnnnCxkk63JN5vCxsPqYX4uF9jSmDHhr8SPJjWFoOm1F_ndaa-GAeoeYGYRgyDKq9YU3CALwICuUEDmYTBNcbIaMqnnFmdWSRR0JnxdCr25wJVL3Qi_N3QMMLEzTU2WO40ShVmjF82mHnpQtwGuhhFfgfRnMCD83K3Lph33qCMx2BwuuARxjKYiZzC4kMDHrjpls37ftAGiCw_cI-gQG3uRyxtvFl9Q_Lg0N14N3vhvFHudhJiesDOlBF-nIhFoHOiMfQzx4iFx7H3wUy-Y0sWWR_fzKkSh-yilyFuv19rCQM9hYQFO9tJjxMI5xTvabXgUp72O2GVqi1A137IqCQWfooHfxctdaTdshuuBSxzl7tdZRMBC3SJV975n7FCXMfN2NKVD5wb-b2YqpiX7kJqvhOKwxPbPMGPyuiNnj98zMMBkYqDcQ4py8qrMZBNNhkQPr5rKbe4tfTrTf2v8kWXIu7zjcV07_EXICOhmv3as0s4dFCFMCQ6zZ5rzsFbVF8_sjTpLmS5KZX_qNxdJXM2_QqyKyUrcj95PCLCWPC6PyZkIAL0jizU09VYNOPntf37Ra3Boqn-b9-Iqd60TW2nMA=w692-h923-s-no?authuser=0'
+                    className='img-fluid rounded shadow-2-strong w-50'
+                    alt='benhome'
+                />
+            </div>
         </>
     );
 }
